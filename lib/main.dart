fd9f25a94c1b8777c585485392c193e56efa0636
JER3MIAH <jeremiahbrownie@gmail.com>
2023-09-16 10:22:54 +0100

    setting up responsive layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/colors.dart';
+import 'package:whatsapp_clone/responsive/responsive_layout.dart';
+import 'package:whatsapp_clone/screens/mobile_screen.dart';
+import 'package:whatsapp_clone/screens/web_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,9 +13,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData.dark(
-        useMaterial3: true,
+      debugShowCheckedModeBanner: false,
+      title: 'Whatsapp Clone',
+      theme: ThemeData.dark(useMaterial3: true)
+          .copyWith(scaffoldBackgroundColor: backgroundColor),
+      home: const ResponsiveLayout(
+        mobileScreenLayout: MobileScreen(),
+        webScreenLayout: WebScreen(),
       ),
     );
   }
