4c4e28799378c03830585c39c552ae0a8beb6c40
JER3MIAH <jeremiahbrownie@gmail.com>
2023-09-18 11:36:38 +0100

    Updating web screen UI

--- lib/screens/web_screen.dart
+++ lib/screens/web_screen.dart
@@ -1,12 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/widgets/contact_list.dart';
 
 class WebScreen extends StatelessWidget {
   const WebScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('Thos is web')),
+    return Scaffold(
+      body: Row(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: Column(
+              children: [
+                // web profile bar
+                // web search bar
+                ContactList(),
+              ],
+            ),
+          ),
+          // Web screen
+          Container(
+            width: MediaQuery.of(context).size.width * .75,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/backgroundImage.png'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
